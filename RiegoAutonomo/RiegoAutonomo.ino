eff870ab600599f4864714cfbb12e0bd28e76322
Pablo Horno <pablojhorno@gmail.com>
2017-08-22 20:03:14 +0200

    Correcion de errores con librerias arduino

--- RiegoAutonomo/RiegoAutonomo.ino
+++ RiegoAutonomo/RiegoAutonomo.ino
@@ -4,19 +4,18 @@
  Author:	Pablo Horno
 */
 #include "Riego.h"
-#include <LiquidCrystal.h>
 
-LiquidCrystal pantalla(12, 11, 5, 4, 3, 2);
+unsigned lastmilis = 0;
 
 void setup() {
-	pinMode(8, OUTPUT);
-	digitalWrite(8, HIGH);
-	pantalla.begin(16, 2);
-	pantalla.print("Iniciando...");
+	Serial.begin(9600);
 }
 
 void loop() {
-	pantalla.clear();
-	pantalla.setCursor(0, 1);
-	pantalla.print(millis() / 1000);
+	unsigned milis = millis() / 10;
+	if (lastmilis != milis)
+	{
+		Serial.println(milis);
+		lastmilis = milis;
+	}
 }
