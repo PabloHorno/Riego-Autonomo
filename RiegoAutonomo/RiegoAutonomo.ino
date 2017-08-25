fbf58469049521af6505e9233283aa182a897083
Pablo Horno <pablojhorno@gmail.com>
2017-08-25 02:08:29 +0200

    Eliminadas referencias a clase tiempo

--- RiegoAutonomo/RiegoAutonomo.ino
+++ RiegoAutonomo/RiegoAutonomo.ino
@@ -3,10 +3,8 @@
  Created:	8/22/2017 5:47:38 PM
  Author:	Pablo Horno
 */
-#include "Tiempo.h"
 #include "Riego.h"
 
-Tiempo tiempo;
 String hora;
 
 void setup() {
@@ -14,10 +12,9 @@ void setup() {
 	Serial.setTimeout(20);
 	Serial.println("Iniciando..");
 	delay(3000);
-	tiempo.init();
 }
 
 void loop() {
-	Serial.println(tiempo.get_hora_str());
+
 	delay(1000);
 }
